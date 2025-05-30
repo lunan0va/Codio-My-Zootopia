8bea3275c0b072ee42a43b714773bfb5770054a3
lunan0va <schnepf.luna@gmail.com>
2025-05-30 15:08:38 +0200

    refactor and enhance animal card layout

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,43 +1,51 @@
 import json
 
+
 def load_data(file_path):
-  """ Load a JSON file """
-  with open(file_path, "r", encoding="utf-8") as handle:
-    return json.load(handle)
+    """Lädt Tierdaten aus einer JSON-Datei."""
+    with open(file_path, "r", encoding="utf-8") as f:
+        return json.load(f)
 
 
-def generate_animal_info(data):
-  output = ''
-  for animal in data:
-    name = animal.get("name")
-    characteristics = animal.get("characteristics", {})
+def serialize_animal(animal_obj):
+    """Serialisiert ein einzelnes Tierobjekt zu HTML."""
+    name = animal_obj.get("name", "")
+    characteristics = animal_obj.get("characteristics", {})
     diet = characteristics.get("diet")
-    locations = animal.get("locations", [])
+    locations = animal_obj.get("locations", [])
     location = locations[0] if locations else None
     type_ = characteristics.get("type")
 
-    output += f'<li class="cards__item"><div class="card__title">{name}</div><p class="card__text">'
+    html = f'<li class="cards__item">'
+    html += f'<div class="card__title">{name}</div>'
+    html += '<p class="card__text">'
     if diet:
-      output += f'<strong>Diet:</strong> {diet}<br/>'
+        html += f'<strong>Diet:</strong> {diet}<br/>'
     if location:
-      output += f'<strong>Location:</strong> {location}<br/>'
+        html += f'<strong>Location:</strong> {location}<br/>'
     if type_:
-      output += f'<strong>Type:</strong> {type_}<br/>'
-    output += '</p></li>'
-  return output
+        html += f'<strong>Type:</strong> {type_}<br/>'
+    html += '</p></li>\n'
+    return html
+
+
+def generate_animal_cards(data):
+    """Erzeugt HTML für alle Tierkarten."""
+    return ''.join(serialize_animal(animal) for animal in data)
 
 
 def create_html(data, template_path, output_path):
-  with open(template_path, "r") as f:
-    html_template = f.read()
+    """Erstellt die finale HTML-Datei mit den Tierkarten."""
+    with open(template_path, "r", encoding="utf-8") as f:
+        template = f.read()
 
-  animal_info = generate_animal_info(data)
-  final_html = html_template.replace("__REPLACE_ANIMALS_INFO__", animal_info)
+    cards_html = generate_animal_cards(data)
+    final_html = template.replace("__REPLACE_ANIMALS_INFO__", cards_html)
 
-  with open(output_path, "w") as f:
-    f.write(final_html)
+    with open(output_path, "w", encoding="utf-8") as f:
+        f.write(final_html)
 
 
 if __name__ == "__main__":
-  animals_data = load_data("animals_data.json")
-  create_html(animals_data, "animals_template.html", "animals.html")
+    data = load_data("animals_data.json")
+    create_html(data, "animals_template.html", "animals.html")
